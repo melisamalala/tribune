54cbfbcaf84b11ea729b38db27666e0b4dab503c
melisamalala <melissamalala@gmail.com>
2018-09-24 18:55:08 +0300

    newsletter recipient class

--- news/views.py
+++ news/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, redirect
-from django.http import HttpResponse, Http404
+from django.http import HttpResponse, Http404, HttpResponseRedirect
 import datetime as dt
-from .models import Article
+from .models import Article, NewsLetterRecipients
 from .forms import NewsLetterForm
 
 
@@ -13,16 +13,21 @@ def welcome(request):
 def news_today(request):
     date = dt.date.today()
     news = Article.todays_news()
-    # day = convert_dates(date)
-
 
     if request.method == 'POST':
         form = NewsLetterForm(request.POST)
         if form.is_valid():
+            name = form.cleaned_data['your_name']
+            email = form.cleaned_data['email']
+            recipient = NewsLetterRecipients(name=name,
+                                             email=email)
+            recipient.save()
+            HttpResponseRedirect('news_today')
             print('valid')
 
     else:
         form = NewsLetterForm()
+
 
     return render(request, 'all-news/today-news.html', {'date': date,
                                                         'news':news,
