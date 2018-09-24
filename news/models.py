d06a7e3b6006fb45c9a63495615f9806a47d5cff
melisamalala <melissamalala@gmail.com>
2018-09-24 15:42:37 +0300

    Added images

--- news/models.py
+++ news/models.py
@@ -1,20 +1,19 @@
 from django.db import models
 import datetime as dt
 
+
 # Create your models here.
-
-
 class Editor(models.Model):
     first_name = models.CharField(max_length=30)
     last_name = models.CharField(max_length=30)
-    email= models.EmailField()
+    email = models.EmailField()
     phone_number = models.CharField(max_length=10, blank=True)
 
     def __str__(self):
         return self.first_name
 
-    class Meta:
-        ordering = ['first_name']
+    class meta:
+        ordering = ['name']
 
     def save_editor(self):
         self.save()
@@ -26,18 +25,22 @@ class tags(models.Model):
     def __str__(self):
         return self.name
 
+
 class Article(models.Model):
     title = models.CharField(max_length=60)
-    post=models.TextField()
-    editor= models.ForeignKey(Editor)
+    post = models.TextField()
+    editor = models.ForeignKey(Editor)
     tags = models.ManyToManyField(tags)
     pub_date = models.DateTimeField(auto_now_add=True)
-    # article_image = models.ImageField(upload_to='articles/')
+    article_image = models.ImageField(upload_to='articles/')
+
+    def __str__(self):
+        return self.title
 
     @classmethod
     def todays_news(cls):
         today = dt.date.today()
-        news=cls.objects.filter(pub_date__date=today)
+        news = cls.objects.filter(pub_date__date=today)
         return news
 
     @classmethod
@@ -49,4 +52,3 @@ class Article(models.Model):
     def search_by_title(cls, search_term):
         news = cls.objects.filter(title__icontains=search_term)
         return news
-
