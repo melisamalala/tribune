4ad90aed33c9ed92dd8200ce0cd22b90044fe330
melisamalala <melissamalala@gmail.com>
2018-09-24 13:41:16 +0300

    Admin site file

--- news/models.py
+++ news/models.py
@@ -8,3 +8,28 @@ class Editor(models.Model):
     last_name = models.CharField(max_length=30)
     email= models.EmailField()
 
+    def __str__(self):
+        return self.first_name
+
+    class Meta:
+        ordering = ['first_name']
+
+    def save_editor(self):
+        self.save()
+
+
+class tags(models.Model):
+    name = models.CharField(max_length=30)
+
+    def __str__(self):
+        return self.name
+
+class Article(models.Model):
+    title = models.CharField(max_length=60)
+    post=models.TextField()
+    editor= models.ForeignKey(Editor)
+    tags = models.ManyToManyField(tags)
+    pub_date = models.DateTimeField(auto_now_add=True)
+
+
+
