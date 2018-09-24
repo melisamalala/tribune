f4d17296aa8d0e631175f54615676f1b1cdf3bab
melisamalala <melissamalala@gmail.com>
2018-09-24 18:38:21 +0300

    Forms for subscription working

--- news/views.py
+++ news/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, redirect
 from django.http import HttpResponse, Http404
 import datetime as dt
 from .models import Article
+from .forms import NewsLetterForm
 
 
 # Create your views here.
@@ -14,7 +15,18 @@ def news_today(request):
     news = Article.todays_news()
     # day = convert_dates(date)
 
-    return render(request, 'all-news/today-news.html', {'date': date, 'news':news})
+
+    if request.method == 'POST':
+        form = NewsLetterForm(request.POST)
+        if form.is_valid():
+            print('valid')
+
+    else:
+        form = NewsLetterForm()
+
+    return render(request, 'all-news/today-news.html', {'date': date,
+                                                        'news':news,
+                                                        'letterForm':form})
 
 
 def convert_dates(dates):
