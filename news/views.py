197b4061043dbb8537af7d0eeb4c830130ec5559
melisamalala <melissamalala@gmail.com>
2018-09-25 11:12:26 +0300

    Registration working

--- news/views.py
+++ news/views.py
@@ -4,6 +4,7 @@ import datetime as dt
 from .models import Article, NewsLetterRecipients
 from .forms import NewsLetterForm
 from .email import send_welcome_email
+from django.contrib.auth.decorators import login_required
 
 
 # Create your views here.
@@ -78,6 +79,8 @@ def search_results(request):
         message = "You haven't searched for any term"
         return render(request, 'all-news/search.html',{"message":message})
 
+
+@login_required(login_url='accounts/login')
 def article(request,article_id):
     try:
         article = Article.objects.get(id = article_id)
